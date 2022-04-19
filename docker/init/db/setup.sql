c620a45148cb2f70f9ae42b471e7f5eb1f4e1a93
motty93 <rdwbocungelt5@gmail.com>
2022-04-19 09:41:05 +0900

    feat: db setting

--- docker/init/db/setup.sql
+++ docker/init/db/setup.sql
@@ -1,5 +1,5 @@
 -- must change your name and databasename, passward.
-CREATE ROLE rails7 LOGIN PASSWORD 'rails7';
-CREATE DATABASE rails7;
-GRANT ALL PRIVILEGES ON DATABASE rails7 TO rails7;
-ALTER ROLE rails7 WITH CREATEROLE CREATEDB;
+CREATE ROLE hotwire_demo LOGIN PASSWORD 'hotwire_demo';
+CREATE DATABASE hotwire_demo;
+GRANT ALL PRIVILEGES ON DATABASE hotwire_demo TO hotwire_demo;
+ALTER ROLE hotwire_demo WITH CREATEROLE CREATEDB;
