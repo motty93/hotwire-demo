4544aa2b0bc3a45d5ca8e90c5f661756d221023d
motty93 <rdwbocungelt5@gmail.com>
2022-07-13 09:18:47 +0900

    feat: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+(1..100).each do |num|
+  Cat.create(name: "cat#{num}", age: num)
+end
